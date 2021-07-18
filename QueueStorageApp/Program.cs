82bcf1ac9446b161dbd3541f8e3b22ceb10e3aa2
karunakarbhogyari <karunakar1008@gmail.com>
2021-07-18 16:53:32 +0530

    Install azure storage queue package ad copy aconnection string

--- QueueStorageApp/Program.cs
+++ QueueStorageApp/Program.cs
@@ -4,6 +4,8 @@ namespace QueueStorageApp
 {
     class Program
     {
+        static string connectionstring = "DefaultEndpointsProtocol=https;AccountName=kpmgtaxstorage;AccountKey=fWKHQsGcHGWTGYBBAugfrWhRpaIUJ6l+uV4dN3/aqxZqrX6iO+TDZflHzzrUNvoejwiaLFBm1gGSE4SfFcFZzQ==;EndpointSuffix=core.windows.net";
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
