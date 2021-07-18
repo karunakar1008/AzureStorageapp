743a3bea19c6cf6b70f995af9fbd2cb996a7622d
karunakarbhogyari <karunakar1008@gmail.com>
2021-07-18 17:10:11 +0530

    Create Queue and insert messages t the queue

---
+++ QueueStorageApp/Order1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueStorageApp
+{
+    class Order1
+    {
+        public string Id { get; set; }
+        public int quantity { get; set; }
+
+        public Order1()
+        {
+            Id = Guid.NewGuid().ToString();
+            Random rnd = new Random();
+            quantity = rnd.Next(1000);
+        }
+
+        public override string ToString()
+        {
+            return $"Id : {Id}, Quantity : {quantity}";
+        }
+    }
+}
--- QueueStorageApp/Program.cs
+++ QueueStorageApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Azure.Storage.Queues;
+using System;
+using System.Threading.Tasks;
 
 namespace QueueStorageApp
 {
@@ -8,7 +10,44 @@ namespace QueueStorageApp
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Queue storage demo");
+
+            //Create queue1 in the Queue storage.
+            CreateQueue();
+
+            CreateQueueAsync().Wait();
+
+            Console.WriteLine("Completed");
         }
+
+        /// <summary>
+        /// Create queue
+        /// </summary>
+        static void CreateQueue()
+        {
+            QueueServiceClient serviceClient = new QueueServiceClient(connectionstring);
+            QueueClient theQueue = serviceClient.CreateQueue("test");
+
+        }
+        /// <summary>
+        /// Create Queue if not exist and insert 20 messages
+        /// </summary>
+        /// <returns></returns>
+        public static async Task CreateQueueAsync()
+        {
+
+            QueueServiceClient queueServiceClient = new QueueServiceClient(connectionstring);
+            QueueClient queueClient = queueServiceClient.GetQueueClient("test");
+            queueClient.CreateIfNotExists();
+
+            for (int i = 1; i <= 20; i++)
+            {
+                Order1 order = new Order1();
+                // await queueClient.SendMessageAsync(order.ToString());
+                await queueClient.SendMessageAsync(order.ToString(), default, TimeSpan.FromSeconds(-1), default); //This message will never expire as we set past time
+            }
+        }
+
+
     }
 }
