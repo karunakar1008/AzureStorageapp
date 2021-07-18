1f36235d0bb6168e0fad2bacb6316f322923b90c
karunakarbhogyari <karunakar1008@gmail.com>
2021-07-18 17:29:20 +0530

    Read the message from queue and pop the message

--- QueueStorageApp/Program.cs
+++ QueueStorageApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
 using System;
 using System.Threading.Tasks;
 
@@ -13,9 +14,11 @@ namespace QueueStorageApp
             Console.WriteLine("Queue storage demo");
 
             //Create queue1 in the Queue storage.
-            CreateQueue();
+            //CreateQueue();
+            //CreateQueueAsync().Wait();
 
-            CreateQueueAsync().Wait();
+            //Read messages
+            ReadMessages();
 
             Console.WriteLine("Completed");
         }
@@ -45,7 +48,33 @@ namespace QueueStorageApp
                 Order1 order = new Order1();
                 // await queueClient.SendMessageAsync(order.ToString());
                 await queueClient.SendMessageAsync(order.ToString(), default, TimeSpan.FromSeconds(-1), default); //This message will never expire as we set past time
+
             }
+        }
+
+        //Read message and pop/delete the message from queue
+        public static void ReadMessages()
+        {
+            //Connect to queue storage
+            QueueServiceClient queueServiceClient = new QueueServiceClient(connectionstring);
+            //var queues = queueServiceClient.GetQueues();
+
+            //get the reference of queue
+            QueueClient queueClient = queueServiceClient.GetQueueClient("test");
+
+            //Azure.Response<QueueMessage[]> responses = queueClient.ReceiveMessages();
+
+            //get the references of top 20 messages in the queue
+            QueueMessage[] retrievedMessage =  queueClient.ReceiveMessages(20);
+            foreach (var message in retrievedMessage)
+            {
+                Console.WriteLine($"Dequeued message: '{message.Body}'");
+
+                // Delete the message
+                queueClient.DeleteMessage(message.MessageId, message.PopReceipt);
+                Console.WriteLine("Message Deleted" + message.MessageId);
+            }
+
         }
 
 
