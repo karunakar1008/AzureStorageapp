3696f1b4b28f85508eecdcaa28db03f42029205a
karunakarbhogyari <karunakar1008@gmail.com>
2021-07-18 17:36:36 +0530

    Delete queue when there are no messages.

--- QueueStorageApp/Program.cs
+++ QueueStorageApp/Program.cs
@@ -18,7 +18,11 @@ namespace QueueStorageApp
             //CreateQueueAsync().Wait();
 
             //Read messages
-            ReadMessages();
+            //ReadMessages();
+
+            //Delete queue if there are no messages
+            QueueClient queueclinet = new QueueClient(connectionstring, "test");
+             RetrieveNextMessageAsync(queueclinet).Wait();
 
             Console.WriteLine("Completed");
         }
@@ -77,6 +81,42 @@ namespace QueueStorageApp
 
         }
 
+
+
+        static async Task<string> RetrieveNextMessageAsync(QueueClient theQueue)
+        {
+            if (await theQueue.ExistsAsync())
+            {
+                QueueProperties properties = await theQueue.GetPropertiesAsync();
+
+                if (properties.ApproximateMessagesCount > 0)
+                {
+                    QueueMessage[] retrievedMessage = await theQueue.ReceiveMessagesAsync(1);
+                    string theMessage = retrievedMessage[0].MessageText;
+                    await theQueue.DeleteMessageAsync(retrievedMessage[0].MessageId, retrievedMessage[0].PopReceipt);
+                    return theMessage;
+                }
+                else
+                {
+                    Console.Write("The queue is empty. Attempt to delete it? (Y/N) ");
+                    string response = Console.ReadLine();
+
+                    if (response.ToUpper() == "Y")
+                    {
+                        await theQueue.DeleteIfExistsAsync();
+                        return "The queue was deleted.";
+                    }
+                    else
+                    {
+                        return "The queue was not deleted.";
+                    }
+                }
+            }
+            else
+            {
+                return "The queue does not exist. Add a message to the command line to create the queue and store the message.";
+            }
+        }
 
     }
 }
