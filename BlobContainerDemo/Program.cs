8f6f5760937047a4289f947b2a2355274837f613
karunakarbhogyari <karunakar1008@gmail.com>
2022-04-07 19:27:55 +0530

    Lease a blob before modifying.

--- BlobContainerDemo/Program.cs
+++ BlobContainerDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
@@ -23,8 +24,8 @@ namespace BlobContainerDemo
             string file2 = "F:\\azure recordings\\az-204\\6. Programming Storage\\images\\pick2.jpg";
             string file3 = "F:\\azure recordings\\az-204\\6. Programming Storage\\images\\pick3.jpg";
 
-            //BlobContainerClient con1 = CreateContainer("con1", false);
-            //BlobContainerClient con2 = CreateContainer("con2", true);
+            BlobContainerClient con1 = CreateContainer("con1", false);
+            BlobContainerClient con2 = CreateContainer("con2", true);
 
             //UploadBlob(con1, file1);
             //UploadBlob(con1, file2);
@@ -33,8 +34,9 @@ namespace BlobContainerDemo
             //ListBlobs(con1);
             //ListBlobs(con2);
             //ListBlobsAsAnonymousUser("con1");
-            ListBlobsAsAnonymousUser("con2");
+            //ListBlobsAsAnonymousUser("con2");
 
+            LeaseDemo(con1);
             Console.ReadLine();
         }
         static BlobContainerClient CreateContainer(string containerName, bool isPublic)
@@ -84,6 +86,46 @@ namespace BlobContainerDemo
                 Console.WriteLine($"{blob.Name} - {blobClient.Uri}");
             }
             Console.WriteLine("");
+        }
+
+        static void LeaseDemo(BlobContainerClient con)
+        {
+            FileInfo fi = new FileInfo("F:\\azure recordings\\az-204\\6. Programming Storage\\demo.txt");
+            AppendBlobClient blob = con.GetAppendBlobClient(fi.Name);
+            blob.CreateIfNotExists(new AppendBlobCreateOptions() { });
+
+            MemoryStream ms = new MemoryStream(System.Text.Encoding.UTF8.GetBytes("\nThis is Line1\n"));
+            blob.AppendBlock(ms);
+            ms.Close();
+
+            BlobLeaseClient lease = blob.GetBlobLeaseClient();
+            lease.Acquire(TimeSpan.FromSeconds(15));
+            Console.WriteLine("Blob lease acquired. Lease = {0}", lease);
+
+            // Update blob without using lease.
+            // This operation will not succeed becuase we acquired lease on the blob and we must specify the lease id.
+            try
+            {
+                ms = new MemoryStream(System.Text.Encoding.UTF8.GetBytes("\nThis is line3\n"));
+                blob.AppendBlock(ms); //Failes because Lease is not mentioned
+                ms.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            // Update blob using lease. This operation will succeed
+            AppendBlobRequestConditions conditions = new AppendBlobRequestConditions()
+            {
+                LeaseId = lease.LeaseId
+            };
+            ms = new MemoryStream(System.Text.Encoding.UTF8.GetBytes("\nThis is Line2\n"));
+            blob.AppendBlock(ms, null, conditions); //Will success as lease is mentioned
+
+            Console.WriteLine("Blob updated using an exclusive lease");
+            ms.Close();
+            lease.Release();
         }
 
     }
